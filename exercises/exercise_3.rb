d08cdf7c81cbdae4f9fc108cc8a9e231e4f87b30
Rob <robpolisena@gmail.com>
2020-08-17 23:13:20 +0000

    updated and completed exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -8,3 +8,12 @@ puts "----------"
 # Your code goes here ...
 @store3 = Store.where(id: 3)
 
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
+puts Store.count
+
+#puts @store3.inspect
+
+puts Store.find_by(id: 3).inspect
+
