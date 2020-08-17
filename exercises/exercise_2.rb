ffdf7661ec104a7b693652737736c8c04d2c3507
Rob <robpolisena@gmail.com>
2020-08-17 23:03:32 +0000

    updated and completed exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -7,11 +7,9 @@ puts "----------"
 # Your code goes here ...
 @store1 = Store.where(id: 1)
 @store2 = Store.where(id: 2)
-@store3 = Store.where(id: 3)
 
 puts @store1.inspect
 puts @store2.inspect
-puts @store3.inspect
 
 @store1 = Store.find_by(name: 'Burnaby')
 @store1.update(name: 'Victoria')
